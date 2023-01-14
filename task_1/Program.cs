751670b05622c0bba395f6deff32b0057b9f5cd0
Kirill Kovalev <kir017@mail.ru>
2023-01-14 21:30:01 +0300

    добавил код для решения задач

---
+++ task_1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Домашнее задание № 6 по курсу: Знакомство с языками программирования (семинары)
+// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] CreateFillArray()
+{
+    Console.Write("Введите количество чисел: ");
+    int[] array = new int[Convert.ToInt32(Console.ReadLine())];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите {i + 1} число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void CountPositiveNumber(int[] colection)
+{
+    int count = 0;
+    for (int i = 0; i < colection.Length; i++)
+    {
+        if (colection[i] > 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"Количество чисел больше 0 -> {count}");
+}
+
+int[] arrayNumbers = CreateFillArray();
+CountPositiveNumber(arrayNumbers);
+
---
+++ task_2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double GetValue(string text)
+{
+    Console.Write(text);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+void DotIntersection(double b1, double b2, double k1, double k2)
+{
+    double y = k1 * (b2 - b1) / (k1 - k2) + b1;
+    double x = (b2 - b1) / (k1 - k2);
+    Console.WriteLine($"Точка пересечения двух прямых -> ({Math.Round(x, 2)};{Math.Round(y, 2)})");
+}
+
+double b1 = GetValue("Введите b1: ");
+double b2 = GetValue("Введите b2: ");
+double k1 = GetValue("Введите k1: ");
+double k2 = GetValue("Введите k2: ");
+DotIntersection(b1, b2, k1, k2);
+
+
+
+
+
+
