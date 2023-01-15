2a6420b67ce3c7dcdabedf741caaa1a15cba224a
Kirill Kovalev <kir017@mail.ru>
2023-01-15 13:23:58 +0300

    подправил код по второй задаче

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -4,15 +4,19 @@
 
 double GetValue(string text)
 {
-    Console.Write(text);
-    return Convert.ToDouble(Console.ReadLine());
+  Console.Write(text);
+  return Convert.ToDouble(Console.ReadLine());
 }
 
 void DotIntersection(double b1, double b2, double k1, double k2)
 {
+  if (b1 != k1 & b2 != k2)
+  {
     double y = k1 * (b2 - b1) / (k1 - k2) + b1;
     double x = (b2 - b1) / (k1 - k2);
     Console.WriteLine($"Точка пересечения двух прямых -> ({Math.Round(x, 2)};{Math.Round(y, 2)})");
+  }
+  else Console.WriteLine("Прямые не пересекаются");
 }
 
 double b1 = GetValue("Введите b1: ");
